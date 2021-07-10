6a2a6b7a017a70622df51c7452aed098a2034c11
Messipsa <im_tamazouzt@esi.dz>
2021-07-10 06:04:14 +0200

    Follow

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -64,3 +64,73 @@ module.exports.updateUser = async (req ,res) =>
     }
 
 };
+
+module.exports.deleteUser = async (req , res) =>
+{
+    if(!ObjectID.isValid(req.params.id))
+    {
+        return res.status(400).send('ID Unknown : ' + req.params.id);
+    }
+
+    try
+    {
+     await UserModel.remove({_id: req.params.id}).exec();
+     res.status(200).json({message : "Successfully deleted"});
+    } catch (err)
+    {
+       return res.status(500).json({message : err});
+    }
+}
+
+module.exports.follow = async(req , res) =>
+{
+    if(!ObjectID.isValid(req.params.id))
+    {
+        return res.status(400).send('ID Unknown : ' + req.params.id);
+    }
+    
+    try
+    {
+
+    }
+    catch(err)
+    {
+        return res.status(500).json({message : err});
+    }
+}
+
+
+module.exports.follow = async(req , res) =>
+{
+    if(!ObjectID.isValid(req.params.id))
+    {
+        return res.status(400).send('ID Unknown : ' + req.params.id);
+    }
+    
+    try
+    {
+       await UserModel.findByIdAndUpdate(
+           req.params.id,
+           {$addToSet : {following : req.body.idToFollow} },
+            {
+                new : true,
+            upsert : true 
+            },
+        (err,data)=>
+        {
+            if(!err)
+            {
+                 res.status(201).json(data);
+            }
+            else
+            {
+                return res.status(400).json({message : err});
+            }
+        }    
+           )
+    }
+    catch(err)
+    {
+        return res.status(500).json({message : err});
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -13,4 +13,10 @@ router.get('/:id' , userController.userInfo);
 
 router.put('/:id' , userController.updateUser);
 
+router.delete('/:id' ,userController.deleteUser);
+
+router.patch('/follow/:id' , userController.follow);
+
+router.patch('/unfollow/:id' , userController.unfollow);
+
 module.exports = router;
